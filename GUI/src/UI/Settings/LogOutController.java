afb2fc60026cd5d05c230f1f7f38fe3472562ea3
zmahmoudzadeh <zmahmoudzadeh78@gmail.com>
2019-07-17 12:24:12 +0430

    GUI returned

---
+++ GUI/src/UI/Settings/LogOutController.java
@@ -0,0 +1,40 @@
+package UI.Settings;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogOutController {
+
+    public Button logOutYesB,logOutNoB;
+    public void YesB() throws IOException {
+        Stage stage = (Stage) logOutYesB.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().
+                getResource("/UI/SignUp/SignUpfx.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage2 = new Stage();
+        Scene scene = new Scene(root);
+        stage2.setScene(scene);
+        stage2.show();
+
+
+    }
+    public void NoB() throws IOException {
+        Stage stage = (Stage) logOutNoB.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().
+                getResource("/UI/Settings/Settingsfx.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage2 = new Stage();
+        Scene scene = new Scene(root);
+        stage2.setScene(scene);
+        stage2.show();
+
+
+    }
+}
